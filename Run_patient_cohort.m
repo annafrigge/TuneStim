e75e1fa0b34fd65567811f3d33cacd7634d427dc
annafrigge <anna.frigge@it.uu.se>
2024-04-26 15:12:17 +0200

    Script for running pipeline on cohorts

---
+++ Run_patient_cohort.m
@@ -0,0 +1,61 @@
+
+
+% Define patient cohort directory
+
+cohort_path = 'C:\Users\annfr888\Documents\DBS\patient_data\Pipeline_study';
+
+% S = dir(cohort_path);
+% 
+% % Initialize an empty cell array to store folder names
+% pat_names = {};
+% 
+% % Loop through each item in the directory contents
+% for i = 1:length(S)
+%     % Check if the item is a directory and not '.' or '..'
+%     if S(i).isdir && ~strcmp(S(i).name, '.') && ~strcmp(S(i).name, '..')
+%         % Add the folder name to the cell array
+%         pat_names{end+1} = S(i).name;
+%     end
+% end
+pat_names = ['DBS_104';'DBS_128';'DBS_133';'DBS_139';'DBS_167';...
+             'DBS_168';'DBS_171';'DBS_185';'DBS_199';'DBS_204'];
+leads = {'S:t Jude 1331','Boston Scientific 2202', 'S:t Jude 1331',...
+         'S:t Jude 1331','Boston Scientific 2202','S:t Jude 1331'...
+         'Boston Scientific 2202','S:t Jude 1331',...
+         'Boston Scientific 2202','Boston Scientific 2202'};
+orientations = {[293,313],[249,288],[12,302],[25,153],[98,193],[52,345],...
+               [32,116],[202,184],16.4,[308,38]};
+atlas = 'DISTAL Minimal (Ewert 2017)';
+target_names = {'STN_motor.nii.gz'};
+constraint_names = {'STN_associative.nii.gz','STN_limbic.nii.gz'};
+optischeme = 'conservative';%'mincov';
+EThreshold = 200;
+relaxation = 10;
+Nthreads = 1;
+space = 'MNI';
+plotoption = 0;
+rebuild = 0;
+
+
+%% Running optimization algorithm of choice for all patients
+for i=9:length(pat_names)
+    disp(append('Patient ',pat_names(i,:),' loading ...'))
+    pat_path = append(cohort_path,filesep,pat_names(i,:),filesep);
+    if strcmp(leads{1,i},'S:t Jude 1331')
+        continue
+    end
+    if strcmp(pat_names(i,:),'DBS_199')
+        hand = {"dx"};
+    else
+        hand = {"sin","dx"};
+    end
+    lead = leads{1,i};
+    lead_orientation = orientations{1,i};
+    for rel = 1:9
+        relaxation = rel*10;
+    main(pat_path,hand,lead,lead_orientation,atlas,target_names,constraint_names,optischeme,EThreshold,relaxation,Nthreads,space,plotoption,rebuild)  
+    end
+
+end
+
+
