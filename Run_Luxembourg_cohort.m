c25492803d4074d98d9137c4f782c0d243d5cfe0
annafrigge <anna.frigge@it.uu.se>
2024-06-05 12:24:29 +0200

    Patient cohort scripts

--- Run_Luxembourg_cohort.m
+++ Run_Luxembourg_cohort.m
@@ -1,6 +1,6 @@
 % Define patient cohort directory
 
-cohort_path = 'C:\Users\annfr888\Documents\DBS\patient_data\Pipeline_study';
+cohort_path = 'C:\Users\annfr888\Documents\DBS\patient_data\2024_Luxembourg_testcases\PilotPatients';
 
 % S = dir(cohort_path);
 % 
@@ -15,18 +15,14 @@ cohort_path = 'C:\Users\annfr888\Documents\DBS\patient_data\Pipeline_study';
 %         pat_names{end+1} = S(i).name;
 %     end
 % end
-pat_names = ['DBS_104';'DBS_128';'DBS_133';'DBS_139';'DBS_167';...
-             'DBS_168';'DBS_171';'DBS_185';'DBS_199';'DBS_204'];
-leads = {'S:t Jude 1331','Boston Scientific 2202', 'S:t Jude 1331',...
-         'S:t Jude 1331','Boston Scientific 2202','S:t Jude 1331'...
-         'Boston Scientific 2202','S:t Jude 1331',...
+pat_names = ['12';'15';'26';'28'];
+leads = {'Boston Scientific 2202','Boston Scientific 2202',...
          'Boston Scientific 2202','Boston Scientific 2202'};
-orientations = {[293,313],[249,288],[12,302],[25,153],[98,193],[52,345],...
-               [32,116],[202,184],16.4,[308,38]};
+orientations = {[25.4,267.1],[133.2,206.9],[0,0],[218.3,268.6]};
 atlas = 'DISTAL Minimal (Ewert 2017)';
 target_names = {'STN_motor.nii.gz'};
 constraint_names = {'STN_associative.nii.gz','STN_limbic.nii.gz'};
-optischeme = 'conservative';%'Ruben';% 'mincov';% 
+optischeme = 'conservative';% 'mincov';%
 EThreshold = 200;
 relaxation = 0:10:90;
 Nthreads = 1;
@@ -36,7 +32,7 @@ rebuild = 0;
 
 
 %% Running optimization algorithm of choice for all patients
-for i=1:length(pat_names)
+for i=1:1%length(pat_names)
     disp(append('Patient ',pat_names(i,:),' loading ...'))
     pat_path = append(cohort_path,filesep,pat_names(i,:),filesep);
     %if strcmp(leads{1,i},'Boston Scientific 2202')
@@ -53,5 +49,3 @@ for i=1:length(pat_names)
     main(pat_path,hand,lead,lead_orientation,atlas,target_names,constraint_names,optischeme,EThreshold,relaxation,Nthreads,space,plotoption,rebuild)  
 
 end
-
-
